85ac37cb39a0f30169d7e9d6188a682e3c0b1841
sanghyub <sanghyub91@gmail.com>
2017-02-07 02:03:00 +0900

    chapter 10

--- CrimianlIntent/app/src/main/java/com/example/sangh/criminalintent/CrimeListFragment.java
+++ CrimianlIntent/app/src/main/java/com/example/sangh/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.sangh.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -30,12 +31,23 @@ public class CrimeListFragment extends Fragment {
         updateUI();
         return view;
     }
+    @Override
+    public void onResume(){
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI(){
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter =new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter==null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        }
+        else{
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -62,7 +74,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v){
-            Toast.makeText(getActivity(),mCrime.getTitle() + " 선택됨!", Toast.LENGTH_SHORT).show();
+           Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
